7cba6facbd1cb6a3627b70b2b824ca1e12a02778
maodus <maodus@ualberta.ca>
2024-12-26 18:30:41 -0700

    chore: tweaked project descriptions

--- src/App.tsx
+++ src/App.tsx
@@ -19,54 +19,63 @@ function App() {
         </p>
         <hr />
       </div>
-      <h1>Projects</h1>
-      <div>
+      <h2>Projects</h2>
+      <div className="project-showcase">
         <ProjectCard
           icon="https://www.svgrepo.com/show/375397/compute-engine.svg"
           title="Gazprea Compiler"
+          demoLink="null"
         >
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            pellentesque ornare quam in feugiat. Aliquam erat volutpat. Morbi
-            mauris est, euismod eu nibh et, luctus ultrices metus. Donec porta
-            eget elit et pretium. Cras finibus, tortor eu sollicitudin semper,
-            ligula lacus tincidunt velit, varius rutrum elit dolor sit amet leo.
-            Cras ut eleifend tellus. Nulla placerat quam eget tortor pretium,
-            sed consequat diam suscipit. Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Duis at lorem a metus pharetra ultrices sed eu
-            magna. Sed consequat suscipit lobortis.
+            This closed-source compiler was built for{" "}
+            <a href="https://cmput415.github.io/415-docs/gazprea/">Gazprea</a>,
+            a language derived from one created at IBM's Hardware Acceleration
+            Lab. The compiler features a modular, heterogeneous AST design and
+            an object-oriented type system supporting scalars, vectors,
+            matrices, and tuples. It includes: type checking with implicit
+            promotion and casting, optimizations like constant folding, and
+            integrates with LLVM for platform-independent code generation.
           </p>
         </ProjectCard>
         <ProjectCard
           icon="https://www.svgrepo.com/show/375469/os-inventory-management.svg"
           title="EZVault"
+          repoLink="https://github.com/CMPUT301F23T21/EZVault"
         >
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            pellentesque ornare quam in feugiat. Aliquam erat volutpat. Morbi
-            mauris est, euismod eu nibh et, luctus ultrices metus. Donec porta
-            eget elit et pretium. Cras finibus, tortor eu sollicitudin semper,
-            ligula lacus tincidunt velit, varius rutrum elit dolor sit amet leo.
-            Cras ut eleifend tellus. Nulla placerat quam eget tortor pretium,
-            sed consequat diam suscipit. Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Duis at lorem a metus pharetra ultrices sed eu
-            magna. Sed consequat suscipit lobortis.
+            EZVault is an open-source inventory management Android app developed
+            in Java using Android Studio. It enables users to efficiently manage
+            inventory, track stock levels, and streamline organization. The app
+            uses Firestore for customer data storage and Firebase Storage for
+            media management. Its CRUD implementation allows for asynchronous
+            communication with Firebase backends, and it also supports barcode
+            scanning for quick item entry.
           </p>
         </ProjectCard>
         <ProjectCard
           icon="https://www.svgrepo.com/show/375408/data-loss-prevention-api.svg"
           title="Who's Gone?"
+          repoLink="https://github.com/maodus/whosgone"
         >
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            pellentesque ornare quam in feugiat. Aliquam erat volutpat. Morbi
-            mauris est, euismod eu nibh et, luctus ultrices metus. Donec porta
-            eget elit et pretium. Cras finibus, tortor eu sollicitudin semper,
-            ligula lacus tincidunt velit, varius rutrum elit dolor sit amet leo.
-            Cras ut eleifend tellus. Nulla placerat quam eget tortor pretium,
-            sed consequat diam suscipit. Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Duis at lorem a metus pharetra ultrices sed eu
-            magna. Sed consequat suscipit lobortis.
+            Who's Gone is a lightweight Python program designed to identify
+            Instagram users who don't follow you back and whom you don't follow.
+            Unlike web-based tools, it handles large datasets without being
+            affected by Instagram's rate limits. Since it runs locally, there's
+            no risk of account flagging or rate-limiting. User's can choose to
+            viualize their results in a minimal HTML file, or via CLI.
+          </p>
+        </ProjectCard>
+        <ProjectCard
+          icon="https://www.svgrepo.com/show/375550/beyondcorp.svg"
+          title="Portfolio Website"
+        >
+          <p>
+            This portfolio website was developed using React and TypeScript,
+            offering a responsive design that ensures a seamless experience
+            across all screen sizes. It leverages React's component-based
+            architecture to create a modular environment, which enhances code
+            maintainability and allows for more efficient development.
           </p>
         </ProjectCard>
         <ProjectCard
