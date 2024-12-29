ec5c09b88f53e195f81971676a123e90d74b8b08
maodus <maodus@ualberta.ca>
2024-12-28 18:17:00 -0700

    feat: comments & support for tech stack in card info type

--- src/types/CardInfo.ts
+++ src/types/CardInfo.ts
@@ -1,7 +1,11 @@
+import {TechName} from "./Technology"
+
+// Type for project card inputs/parameters
 export type CardInfo = {
-  title: string
-  icon: string
-  demoLink?: string
-  repoLink?: string
-  children?: React.ReactElement | React.ReactElement[]
+  title: string // Project name
+  icon: string // Icon path
+  demoLink?: string // Optional link to youtube
+  repoLink?: string // Optional link to github
+  children?: React.ReactElement | React.ReactElement[] // Optional DOM elements for the body of the card
+  tech?: TechName[] // Optional technology stack
 }
